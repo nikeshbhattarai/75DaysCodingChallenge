905a701af57e5de119668842c6c359c823afb3d4
nikesh <nikeshvtrai10@gmail.com>
2022-07-18 14:28:24 +0700

    Simple string practice

---
+++ Day1/BandNameGenerator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator.")
+city = input("Write the name of the city you grew up in.\n")
+pet_name = input("Write the name of your pet.\n")
+
+band_name = city +" "+ pet_name
+print("Your band name could be " + band_name)
