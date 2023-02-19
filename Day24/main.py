f46961354bc5fb65937368efb1c680d293ecb2b2
Nikesh Bhattarai <nikeshvtrai10@gmail.com>
2023-02-19 23:13:22 +0700

    Learned how to read and write files and Created a file to keep track of highscore.

--- Day24/main.py
+++ Day24/main.py
@@ -1,4 +1,6 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("new_file.txt", "w") as file:
+    file.write("New text.")
