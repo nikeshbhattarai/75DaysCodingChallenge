dd655fd61622b42d3710ccd426c8fa412066ffe8
Nikesh Bhattarai <nikeshvtrai10@gmail.com>
2023-03-23 23:15:45 +0700

    Learned about tkinter grid and completed the exercise.

--- Day27/main.py
+++ Day27/main.py
@@ -12,15 +12,19 @@ window.minsize(width=500, height=300)
 
 #Label
 my_label = Label(text="This is a label", font=("Arial", 24, "bold"))
-my_label.pack(side='left')
+my_label.config(text="Label")
+my_label.grid(column=0, row=0)
 
 #Button
-button = Button(text="Click me", command=button_clicked)
-button.pack(side='left')
+button = Button(text="Button", command=button_clicked)
+button.grid(column=1, row=1)
 
+# New Button 
+new_button = Button(text="New Button", command=button_clicked)
+new_button.grid(column=2, row=0)
 #Entry
-input = Entry(width=10)
-input.pack(side="left")
+input = Entry(text="Entry", width=10)
+input.grid(column=3, row=3)
 
 
 window.mainloop()
