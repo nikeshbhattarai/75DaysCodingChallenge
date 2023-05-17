6900cf2e931628535a1b140e017d0f9eb733b46e
Nikesh Bhattarai <nikeshvtrai10@gmail.com>
2023-05-17 12:09:52 +0700

    Created a basic app to display Kanye quotes by fetching the kanye rest API.

---
+++ Day33/main.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+longitude = response.json()["iss_position"]["longitude"]
+latitude =  response.json()["iss_position"]["latitude"]
+
