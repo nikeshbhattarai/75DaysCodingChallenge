e99a2aceeef28c35653e63538ab312088a3e25a9
Nikesh Bhattarai <nikeshvtrai10@gmail.com>
2023-06-02 00:10:21 +0700

    Completed all tasks until step 2.

--- Day39/flight-deals-start/data_manager.py
+++ Day39/flight-deals-start/data_manager.py
@@ -1,4 +1,21 @@
+import requests
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/75327416cb3e93bd5260e25a6055c289/flightDeals/prices/"
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        pass
+        self.destination_data = {}
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+               "price": {
+                   "iataCode": city["iataCode"]
+               } 
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)        
